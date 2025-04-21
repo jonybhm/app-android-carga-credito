9a3558b5376fc121e906df923a730ffcc79d6515
unknown <jony.bhm@gmail.com>
2025-04-21 19:23:57 -0300

    first push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+
+    apiKey: "AIzaSyASYUnU7z7doTMpM058Ylw96x4b6tO0Z50",
+  
+    authDomain: "carga-credito-4f99c.firebaseapp.com",
+  
+    projectId: "carga-credito-4f99c",
+  
+    storageBucket: "carga-credito-4f99c.firebasestorage.app",
+  
+    messagingSenderId: "397201454124",
+  
+    appId: "1:397201454124:web:fac95e80c908cbbae08d89"
+  
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,22 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+
+    apiKey: "AIzaSyASYUnU7z7doTMpM058Ylw96x4b6tO0Z50",
+  
+    authDomain: "carga-credito-4f99c.firebaseapp.com",
+  
+    projectId: "carga-credito-4f99c",
+  
+    storageBucket: "carga-credito-4f99c.firebasestorage.app",
+  
+    messagingSenderId: "397201454124",
+  
+    appId: "1:397201454124:web:fac95e80c908cbbae08d89"
+  
+  }
 };
 
 /*
